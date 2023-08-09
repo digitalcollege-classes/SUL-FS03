26fd1ae9ed5368dc9290f874c3cd2b03b4167f54
Wagnermorais01 <moraiswg@hotmail.com>
2023-08-08 22:30:40 -0300

    praticas

---
+++ m2/u1/a02/Wagner Morais/index.js
@@ -0,0 +1,9 @@
+console.log('> vamos imprimir os números de 0 a 10')
+for (let contador = 0; contador <= 10; contador = contador + 1) {
+    console.log('o número atual é o:', contador)
+    console.log(contador)
+    console.log('é o dobro dele é:')
+    console.log(contador*2)
+  }
+
+ 
