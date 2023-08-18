201f2950218fdc8eaefbda00e74aa4f6f362c262
Eduardo Lima <eduardolima.cel23@gmail.com>
2023-08-17 22:18:27 -0300

    treinando commits, macho!

---
+++ m2/u1/a03/Eduardo lima/index.js
@@ -0,0 +1,11 @@
+var passo;
+for (passo = 0; passo < 5; passo++) {
+  // Executa 5 vezes, com os valores de passos de 0 a 4.
+  console.log("Este é o passo: ", passo)
+  console.log("Ande um passo para o leste");
+}
+
+// function quadrado (numero) {
+//     return numero * numero ;
+// }
+// let valor = quadrado(2)
