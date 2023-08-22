a39ea12a71770e1eb1cb066bfd5c2e74322d4517
William Correa <student@wilcorrea.dev>
2023-08-22 20:08:21 -0300

    feature: material das aulas

---
+++ m2/u1/a04/digitalcolleger/index.js
@@ -0,0 +1,10 @@
+
+function solucao(valor) {
+    // Executa 5 vezes, com os valores de passos de 0 a 4.
+    console.log("Este é o passo: ", valor)
+    console.log("Ande um passo para o leste");
+}
+
+for (let passo = 0; passo < 5; passo++) {
+    solucao(passo)
+}
