abb5112f70b8468f9d11828de5498b81c0dc4ddf
AndreGirao <andreprogramacao600@gmail.com>
2023-10-10 20:42:12 -0300

    exercicio personagens

---
+++ AULAS ELIAS/script.js
@@ -0,0 +1,23 @@
+const carro = [
+    [
+        modelo: "Gol"
+        ano: "2012"
+        cor: "vermelho"
+
+    ],
+    [
+        modelo: "Fusca"
+        ano: "1980"
+        cor: "Azul"
+
+    ],
+    [
+        modelo: "Gurgel"
+        ano: "1990"
+        cor: "Branco"
+
+    ]
+]
+
+function chamaCarro
+console.log()
